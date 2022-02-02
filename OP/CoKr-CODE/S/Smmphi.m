a65558c601d77d3bd5b26adebd9d6817e0c71b48
cvk-m <cevik.mte@gmail.com>
2022-02-02 22:30:44 +0300

    Create Smmphi.m

---
+++ OP/CoKr-CODE/S/Smmphi.m
@@ -0,0 +1,29 @@
+function Phiq=Smmphi(X, q, p)
+% Calculates the sampling plan quality criterion of Morris and
+% Mitchell.
+%
+% Inputs:
+% X – sampling plan
+% q – exponent used in the calculation of the metric
+% p – the distance metric to be used (p=1 rectangular –
+% (default, p=2 Euclidean)
+%
+% Output:
+% Phiq – sampling plan ‘space–fillingness’ metric
+% Assume defaults if arguments list incomplete
+if exist('p','var')
+    p=p;
+else
+p=1;
+end
+if exist('q','var')
+    p=p;
+else
+q=2;
+end
+% Calculate the distances between all pairs of
+% points (using the p–norm) and build multiplicity array J
+%
+[J,d]=Sjd(X,p);
+% The sampling plan quality criterion
+Phiq=sum(J.*(d.^(-q)))^(1/q);
